7333592fbaa28a60d3990c282a5a38fb99b2c1be
Brandon Knox <bknox.dev@gmail.com>
2021-04-25 18:08:07 -0700

    Removed CoreData files from previous example.  Added example of using moc.hasChanges to check for changes before writing changes to memory

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @Environment(\.managedObjectContext) var moc
+    
     var body: some View {
-        Text("Hello World")
+        Button("Save") {
+            if self.moc.hasChanges {
+                try? self.moc.save()
+            }
+        }
     }
 }
 
