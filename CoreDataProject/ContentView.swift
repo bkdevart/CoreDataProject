b360e213ffcc3257590dcb1a654478b2d5e086a7
Brandon Knox <bknox.dev@gmail.com>
2021-04-25 18:01:23 -0700

    Created movie entity in CoreData, and generated NSManaged subclass (2 files) for entity

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,22 +7,15 @@
 
 import SwiftUI
 
-struct Student: Hashable {
-    let name: String
-}
-
 struct ContentView: View {
-    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
 
     var body: some View {
-        List(students, id: \.self) { student in
-            Text(student.name)
-        }
+        Text("Hello World")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Brandon Knox on 4/25/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
