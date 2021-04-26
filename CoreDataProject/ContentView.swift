b0d07c003cb22aa189e2fb7850c447d3af6bd609
Brandon Knox <bknox.dev@gmail.com>
2021-04-25 17:52:34 -0700

    Example of .self working with hashable, since swift generates a unique identifier with each hash

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -6,15 +6,17 @@
 //
 
 import SwiftUI
-import CoreData
+
+struct Student: Hashable {
+    let name: String
+}
 
 struct ContentView: View {
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
 
     var body: some View {
-        List {
-            ForEach([2, 4, 6, 8, 10], id: \.self) {
-                Text("\($0) is even")
-            }
+        List(students, id: \.self) { student in
+            Text(student.name)
         }
     }
 }
